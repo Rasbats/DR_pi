0f6615082b594e1bdb8367551968485f4b2c2ad7
Rasbats <miker@members.swis.net>
2022-06-29 07:10:19 +0100

    Templates: Updating ci

--- ci/circleci-build-flatpak.sh
+++ ci/circleci-build-flatpak.sh
@@ -12,69 +12,70 @@
 # (at your option) any later version.
 
 set -e
-
 MANIFEST=$(cd flatpak; ls org.opencpn.OpenCPN.Plugin*yaml)
 echo "Using manifest file: $MANIFEST"
 set -x
 
-sudo apt update
+# Load local environment if it exists i. e., this is a local build
+if [ -f ~/.config/local-build.rc ]; then source ~/.config/local-build.rc; fi
+if [ -d /ci-source ]; then cd /ci-source; fi
 
+git submodule update --init opencpn-libs
 
-# Avoid using outdated TLS certificates, see #210.
-sudo apt install --reinstall  ca-certificates
+# Set up build directory and a visible link in /
+builddir=build-flatpak
+test -d $builddir || sudo mkdir $builddir && sudo rm -rf $builddir/*
+sudo chmod 777 $builddir
+if [ "$PWD" != "/"  ]; then sudo ln -sf $PWD/$builddir /$builddir; fi
 
-# Install flatpak and flatpak-builder
-sudo apt install flatpak flatpak-builder
+# Create a log file.
+exec > >(tee $builddir/build.log) 2>&1
+
+if [ -n "$TRAVIS_BUILD_DIR" ]; then cd $TRAVIS_BUILD_DIR; fi
+
+if [ -n "$CI" ]; then
+    sudo apt update
+
+    # Avoid using outdated TLS certificates, see #210.
+    sudo apt install --reinstall  ca-certificates
+
+    # Install flatpak and flatpak-builder
+    sudo apt install flatpak flatpak-builder
+fi
+
+# The flatpak checksumming needs python3:
+if ! python3 --version 2>&1 >/dev/null; then
+    pyenv local $(pyenv versions | sed 's/*//' | awk '{print $1}' | tail -1)
+    cp .python-version $HOME
+fi
+
+flatpak remote-add --user --if-not-exists flathub-beta \
+    https://flathub.org/beta-repo/flathub-beta.flatpakrepo
 flatpak remote-add --user --if-not-exists \
     flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-# aarch64 is built from beta branch, regular x86_64 from stable.
-# Both uses 20.08. Compatibility 18.08 builds are built for
-# x86_64 only using last known 18.08 commit on stable branch.
-
-commit_1808=959f5fd700f72e63182eabb9821b6aa52fb12189eddf72ccf99889977b389447
-FLATPAK_BRANCH=stable
-if dpkg-architecture --is arm64; then
-    flatpak install --user -y --noninteractive \
-        flathub org.freedesktop.Sdk//20.08
-    flatpak remote-add --user --if-not-exists flathub-beta \
-        https://flathub.org/beta-repo/flathub-beta.flatpakrepo
-    flatpak install --user -y --or-update  --noninteractive \
-        flathub-beta org.opencpn.OpenCPN
-    FLATPAK_BRANCH=beta
-elif [ -n "$BUILD_1808" ]; then
-    flatpak install --user -y --noninteractive \
-        flathub org.freedesktop.Sdk//18.08
-    flatpak install --user -y --or-update --noninteractive \
-        flathub  org.opencpn.OpenCPN
-    flatpak update --user -y --noninteractive --commit $commit_1808 \
-        org.opencpn.OpenCPN
-    sed -i '/sdk:/s/20.08/18.08/'  flatpak/org.opencpn.*.yaml
-else
-    flatpak install --user -y --noninteractive \
-        flathub org.freedesktop.Sdk//20.08
-    flatpak install --user -y --or-update --noninteractive \
-        flathub  org.opencpn.OpenCPN
-fi
-
-# Patch the runtime version so it matches the nightly builds
-# or beta as appropriate.
-sed -i "/^runtime-version/s/:.*/: $FLATPAK_BRANCH/" flatpak/$MANIFEST
-
-# The flatpak checksumming needs python3:
-pyenv local $(pyenv versions | sed 's/*//' | awk '{print $1}' | tail -1)
-cp .python-version $HOME
+flatpak install --user -y --noninteractive \
+    flathub org.freedesktop.Sdk//20.08
 
 # Configure and build the plugin tarball and metadata.
-mkdir build; cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cd $builddir
+if [ -n "$BUILD_WX31" ]; then
+    manifest=$(ls ../flatpak/org.opencpn.OpenCPN.Plugin*yaml)
+    sed -i  '/STRING=TARBALL/s/$/ -DOCPN_WX_ABI=wx315/' $manifest
+    sed -i  '/runtime-version/s/stable/beta/'  $manifest
+    flatpak install --user -y --or-update --noninteractive \
+        flathub-beta  org.opencpn.OpenCPN//beta
+    cmake -DCMAKE_BUILD_TYPE=Release -DOCPN_WX_ABI=wx315  ..
+else
+    flatpak install --user -y --or-update --noninteractive \
+        flathub  org.opencpn.OpenCPN
+    cmake -DCMAKE_BUILD_TYPE=Release ..
+fi
 make -j $(nproc) VERBOSE=1 flatpak
 
-# Fix upload script if building 18.08:
-test -n "$BUILD_1808" && sed -i 's/20.08/18.08/' upload.sh
-
-# Restore patched file so the cache checksumming is ok.
-git checkout ../flatpak/$MANIFEST
+# Restore permissions and owner in build tree.
+if [ -d /ci-source ]; then sudo chown --reference=/ci-source -R . ../cache; fi
+sudo chown --reference=.. .
 
 # Install cloudsmith and cryptography, required by upload script and git-push
 python3 -m pip install -q --user --upgrade pip
--- ci/circleci-build-macos.sh
+++ ci/circleci-build-macos.sh
@@ -13,6 +13,18 @@
 
 set -xe
 
+# Load local environment if it exists i. e., this is a local build
+if [ -f ~/.config/local-build.rc ]; then source ~/.config/local-build.rc; fi
+
+git submodule update --init opencpn-libs
+
+# Set up build directory
+if [ -n "$TRAVIS_BUILD_DIR" ]; then cd $TRAVIS_BUILD_DIR; fi
+rm -rf build-osx  && mkdir build-osx
+
+# Create a log file.
+exec > >(tee build-osx/build.log) 2>&1
+
 export MACOSX_DEPLOYMENT_TARGET=10.10
 
 # Return latest version of $1, optiomally using option $2
@@ -29,6 +41,8 @@ for pkg in $(sed '/#/d' < $here/../build-deps/macos-deps);  do
     brew link --overwrite $pkg || brew install $pkg
 done
 
+export OPENSSL_ROOT_DIR='/usr/local/opt/openssl'
+
 # Install the pre-built wxWidgets package
 
 wget -q https://download.opencpn.org/s/MCiRiq4fJcKD56r/download \
@@ -36,7 +50,7 @@ wget -q https://download.opencpn.org/s/MCiRiq4fJcKD56r/download \
 tar -C /tmp -xJf /tmp/wx315_opencpn50_macos1010.tar.xz
 
 # Build and package
-rm -rf build && mkdir build && cd build
+cd build-osx
 cmake \
   -DCMAKE_BUILD_TYPE=Release \
   -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wx315_opencpn50_macos1010/bin/wx-config \
@@ -47,20 +61,20 @@ cmake \
 
 if [[ -z "$CI" ]]; then
     echo '$CI not found in environment, assuming local setup'
-    echo "Complete build using 'cd build; make tarball' or so."
+    echo "Complete build using 'cd build-osx; make tarball' or so."
     exit 0
 fi
 
 make VERBOSE=1 tarball
 
 # Install cloudsmith needed by upload script
-python3 -m pip install --user cloudsmith-cli
+python3 -m pip install -q --user cloudsmith-cli
 
 # Required by git-push
-python3 -m pip install --user cryptography
+python3 -m pip install -q --user cryptography
 
 # python3 installs in odd place not on PATH, teach upload.sh to use it:
 pyvers=$(python3 --version | awk '{ print $2 }')
 pyvers=$(echo $pyvers | sed -E 's/[\.][0-9]+$//')    # drop last .z in x.y.z
-echo "export PATH=\$PATH:/Users/distiller/Library/Python/$pyvers/bin" \
-    >> ~/.uploadrc
+py_dir=$(ls -d  /Users/*/Library/Python/$pyvers/bin)
+echo "export PATH=\$PATH:$py_dir" >> ~/.uploadrc
