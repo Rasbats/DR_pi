7312fc650e2eb0e8fe87c6123c6fb33dc5ecffdb
Rasbats <miker@members.swis.net>
2023-10-10 17:39:15 +0100

    start point

--- src/DRgui_impl.cpp
+++ src/DRgui_impl.cpp
@@ -124,8 +124,8 @@ void Dlg::OnMouseEvent(wxMouseEvent& event)
 
                 wxSize dragSize = m_resizeStartSize;
 
-                dragSize.y = p.y; //  -m_resizeStartPoint.y;
-                dragSize.x = p.x; //  - m_resizeStartPoint.x;
+                dragSize.y += p.y -m_resizeStartPoint.y;
+                dragSize.x += p.x - m_resizeStartPoint.x;
                 ;
                 /*
                 if ((par_pos.y + dragSize.y) > par_size.y)
@@ -146,8 +146,8 @@ void Dlg::OnMouseEvent(wxMouseEvent& event)
 
                 wxSize dragSize = m_resizeStartSize;
 
-                dragSize.y = p.y;//                -m_resizeStartPoint.y;
-                dragSize.x = p.x;//                -m_resizeStartPoint.x;
+                dragSize.y += p.y -m_resizeStartPoint.y;
+                dragSize.x += p.x -m_resizeStartPoint.x;
                 ;
                 /*
                 if ((par_pos.y + dragSize.y) > par_size.y)
