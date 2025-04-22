a9faf163bfd9d546676cc85f002bb525fe5a55ee
Rasbats <miker@members.swis.net>
2025-04-22 10:13:55 +0100

    ifdef

--- src/DR_pi.h
+++ src/DR_pi.h
@@ -43,6 +43,14 @@
 
 #include "config.h"
 
+#ifdef __ANDROID__
+#include <qopengl.h>
+typedef double GLdouble;
+#define GL_GLEXT_LEGACY 1
+#include "GLES2/gl2.h"
+#include "glu_gl.h"
+#include "GL/glu.h"
+#endif
 
 //----------------------------------------------------------------------------------------------------------
 //    The PlugIn Class Definition
